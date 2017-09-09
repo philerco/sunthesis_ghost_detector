c3ba948d8951099587143ec8c7373d96687e628d
Philippe Ercolessi <philippe@admo.tv>
2017-09-09 15:25:45 +0200

    Programme test com GPIO raspberry et arduino

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include "src/phidget/PhidgetDigitalOutput.h"
 #include "src/phidget/PhidgetVoltageInput.h"
+#include "src/RPi/GPIOClass.h"
 
 using namespace std;
 
@@ -14,53 +15,23 @@ int main()
 {
     cout << "Hello World!" << endl;
 
-    vector<PhidgetDigitalOutput> leds(8);
+    //Init sound GPIO pin
+    GPIOClass* mSound;
+    mSound = new GPIOClass();
+    mSound->export_gpio();
+    mSound->setdir_gpio("out");
 
-    //////////////////////////////////////////////////////////////////
-    //Simulate system init (light all leds and deactivate them like k2000)
-    for(int i=0; i < 8; i++){
-        leds[i].init(i);
-        leds[i].activate();
-        usleep(20000);
-    }
-    for(auto& led:leds){
-        led.deactivate();
-        usleep(20000);
-    }
-    //End Init
-    //////////////////////////////////////////////////////////////////
-
-    //Init voltage sensors
-    PhidgetVoltageInput rotationSensor;
-    rotationSensor.init(0);
-
-    PhidgetVoltageInput voltageSensor;
-    voltageSensor.init(1);
-
-    //Init RPi GPIO
-
-
-    double voltage = 1;
-    //voltage = rotationSensor.currentVoltage();
-    sleep(1);
-    while(voltage > 0.0001){
-        voltage = rotationSensor.currentVoltage();
-
-        //get nbled to light
-        int nbled = voltage*8/5;
-        for(int i = 0; i < nbled; ++i){
-            leds[i].activate();
+    int cptr=20;
+    int sleep = 2000000;
+    while(cptr-->0){
+        if(cptr%2 == 0){
+            mSound->setval_gpio("1");
+        }else{
+            mSound->setval_gpio("0");
         }
-        for(int i = nbled; i < 8; ++i){
-            leds[i].deactivate();
-        }
+        usleep(sleep);
+        sleep /=2;
     }
-
-    for(auto& led:leds){
-        led.release();
-    }
-
-    cout << "end release" << endl;
 
     return 0;
 }
